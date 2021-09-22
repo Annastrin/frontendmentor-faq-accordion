7a9b6ad33a7f84761a08d2aeb220f8eca1d20021
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2021-09-22 22:02:18 +1200

    Clean up webpack config, dependencies and index.pug

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,19 @@
-const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
       template: "./index.pug",
-      inject: true,
       favicon: "./favicon.ico",
     }),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-    }),
+    new MiniCssExtractPlugin(),
   ],
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: [
-          // fallback to style-loader in development
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
-        ],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.pug$/,
@@ -40,14 +22,6 @@ module.exports = {
             loader: "pug-loader",
           },
         ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
       },
     ],
   },
